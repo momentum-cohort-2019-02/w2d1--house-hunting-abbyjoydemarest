0edd7fea9753251c16a30a3dd48ac96eaaa1dcea
Abby Demarest <abbyjoydemarest@gmail.com>
2019-02-19 13:08:36 -0500

    Changed after class. Did hard mode :)

--- house-hunting.py
+++ house-hunting.py
@@ -1,29 +1,36 @@
 portion_down_payment = float(0.25)
 current_savings = int(0)
-r = float(0.04)
+annual_return = float(0.04)
+month = 0
 
-annual_salary = True
-while annual_salary:
-    print('Yay! You are considering buying a house!' +
-    'Please answer the following questions with whole numbers' + '(or decimals when requested).')
-    annual_salary = int(input('What is your annual salary? '))
-    if annual_salary != "":
-        monthly_salary = int(annual_salary / 12)
-        print(monthly_salary)
-        portion_saved = monthly_salary * float(input('What portion of your salary do you want saved? (Provide as a decimal) '))
-    if portion_saved != "":
-        print('portion-saved' + str(portion_saved))
-        annual_portion_saved = int((portion_saved * 12))
-        current_savings = int(annual_portion_saved * r)
-        print('current-savings' + str(current_savings))
-        total_cost = int(input('What is the total cost of your dream home? '))
-    if total_cost != "":
-        print('total-cost' + str(total_cost))
-        down_payment = int(total_cost * portion_down_payment)
-    if down_payment > 0:
-        print('down-payment' + str(down_payment))
-        annual_savings = current_savings + annual_portion_saved
-        number_of_years_annual_savings = down_payment / annual_savings
-        number_of_months = int(number_of_years_annual_savings) * 12
-        print('Number of months: ' + str(number_of_months))
+print("Yay! You are considering buying a house!"
+"Please answer the following questions with whole numbers" 
+"or decimals when requested).")
+annual_salary = float(input('Enter your annual salary: '))
+portion_saved = float(input('Enter the percent of your salary to save, as a decimal: '))
+annual_return = input('Enter the expected annual rate of return: ')
+if annual_return != "":
+    annual_return = 0.04
+total_cost = float(input('Enter the cost of your dream home: '))
+portion_down_payment = (input("Enter the percent of your home's cost to save as a down payment: "))
+if portion_down_payment != "":
+    portion_down_payment = 0.25
+monthly_salary = (annual_salary / 12)
+print("monthly-salary" + str(monthly_salary))
+
+monthly_portion_saved = portion_saved * monthly_salary
+print( "monthly-portion-saved" + str(monthly_portion_saved))
+
+down_payment = float(total_cost) * float(portion_down_payment)
+print( "down-payment" + str(down_payment))
+
+
+current_savings= 0
+current_month = 0
+while current_savings < down_payment:
+    current_month += 1
+    interest = current_savings * float(annual_return) / 12
+    current_savings = current_savings + monthly_portion_saved + interest
+print("Number of months: " + str(current_month))
+
 
